9bd9d6e7cf2e7f81ecfa9c87859d94c03161f8bf
Nareik2610 <Nareik.2610@gmx.de>
2025-01-10 15:56:15 +0100

    10.01.2025

--- arraytools/arraytools.go
+++ arraytools/arraytools.go
@@ -4,10 +4,22 @@ package arraytools
 // Multipliziert jedes Element des Arrays mit dem Faktor.
 func ScalarMult(a []float64, factor float64) {
 	// TODO
+
+	for pos, wert := range a {
+
+		a[pos] = wert * factor
+
+	}
+
 }
 
 // Add erwartet zwei Arrays der gleichen Länge.
 // Addiert die Elemente der Arrays paarweise.
 func Add(a, b []float64) {
-	// TODO
+
+	for pos := range a {
+
+		a[pos] += b[pos]
+	}
+
 }
--- matrixtools/matrixtools.go
+++ matrixtools/matrixtools.go
@@ -1,15 +1,26 @@
 package matrixtools
 
+import "matrices/arraytools"
+
 // GetRow liefert die i-te Zeile der Matrix m.
 func GetRow(m [][]float64, i int) []float64 {
 	// TODO
-	return []float64{}
+
+	result := m[i]
+
+	return result
 }
 
 // GetCol liefert die j-te Spalte der Matrix m.
 func GetCol(m [][]float64, j int) []float64 {
 	col := make([]float64, len(m))
 	// TODO
+	for i := range m {
+
+		col[i] = m[i][j]
+
+	}
+
 	return col
 }
 
@@ -17,12 +28,14 @@ func GetCol(m [][]float64, j int) []float64 {
 // Addiert die beiden Zeilen paarweise und speichert das Ergebnis in der ersten Zeile.
 func AddRows(m [][]float64, i, j int) {
 	// TODO
+	arraytools.Add(m[i], m[j])
 }
 
 // ScalarMultRow erwartet eine Matrix, eine Zeilennummer und einen skalaren Faktor.
 // Multipliziert die Zeile mit dem Faktor und speichert das Ergebnis in der Zeile.
 func ScalarMultRow(m [][]float64, i int, factor float64) {
 	// TODO
+	arraytools.ScalarMult(m[i], factor)
 }
 
 // Transpose erwartet eine Matrix und liefert ihre Transponierte.
@@ -30,5 +43,9 @@ func ScalarMultRow(m [][]float64, i int, factor float64) {
 func Transposed(m [][]float64) [][]float64 {
 	transposed := make([][]float64, len(m[0]))
 	// TODO
+	for i := range m[0] { //tansposed = len(m[0]
+
+		transposed[i] = GetCol(m, i)
+	}
 	return transposed
 }
